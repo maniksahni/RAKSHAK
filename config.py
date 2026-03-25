78dfe82dd3e1ba84f4b868456d901fc8ecaae422
maniksahni <maniksahni5@gmail.com>
2026-03-25 21:47:47 +0530

    fix(ratelimit): substantially increase rate limits to prevent False Positives via Proxy on Railway

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
 
     # Rate limiting
     RATELIMIT_STORAGE_URL = 'memory://'
-    RATELIMIT_DEFAULT = '200 per day;50 per hour'
+    RATELIMIT_DEFAULT = '20000 per day;5000 per hour'
 
     # SocketIO
     SOCKETIO_ASYNC_MODE = 'eventlet'
