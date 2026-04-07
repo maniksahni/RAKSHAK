14402264febbdcca000d64054a56ae5f8e46fc2d
maniksahni <maniksahni5@gmail.com>
2026-04-08 03:49:07 +0530

    chore: Remove hand gesture feature

--- modules/vision_shield/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from flask_login import login_required
-from modules.vision_shield import vision_shield_bp
-
-@vision_shield_bp.route('/')
-@login_required
-def index():
-    return render_template('vision_shield/index.html')
